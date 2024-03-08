4df0f9f2d512b65778a488da7f70ef4bd96f598a
Guilherme Lúcio <guilhermecaixeta@users.noreply.github.com>
2024-03-08 12:45:29 +0000

    Feat/improve roles defined permissions (#14)
    * Remove unecessary JS for modals
    
    * fixing forms modal errors
    
    * fix new password link
    
    * Update fronted
    
    # Changes
    - Adding nested partials for commons features
    - removing unnecessary js returns
    - adding login modal for site
    - removing unnecessary controllers
    - change key translations for clarity
    - refactor html to be more strict and dry
    - adjust routes
    - adding missing translations
    
    * Update devcontainer
    
    * Update task to set permissions for roles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,10 +47,42 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
   # Authorization
+  config.default_permissions = ["#{Backoffice::AdminsController.controller_path}:write",
+                                "#{Backoffice::AdminsController.controller_path}:read",
+                                "#{Backoffice::CategoriesController.controller_path}:read",
+                                "#{Backoffice::CategoriesController.controller_path}:write",
+                                "#{Backoffice::MembersController.controller_path}:write",
+                                "#{Backoffice::MembersController.controller_path}:read",
+                                "#{Backoffice::RolesController.controller_path}:write",
+                                "#{Backoffice::RolesController.controller_path}:read",
+                                "#{Backoffice::PermissionsController.controller_path}:read",
+                                "#{Backoffice::DashboardController.controller_path}:read",
+                                "#{Backoffice::MessageController.controller_path}:write",
+                                "#{Devise::SessionsController.controller_path}:write",
+                                "#{Site::HomeController.controller_path}:read"]
   config.default_roles = [
-    { name: "Administrator", is_admin: true, is_opt: false, is_member: false },
-    { name: "Operator", is_admin: false, is_opt: true, is_member: false },
-    { name: "Member", is_admin: false, is_opt: false, is_member: true },
+    { name: "Administrator",
+      is_admin: true,
+      is_opt: false,
+      is_member: false,
+      permissions: ["*"],
+      except_permissions: ["#{Backoffice::MembersController.controller_path}:write"] },
+    { name: "Operator",
+     is_admin: false,
+     is_opt: true,
+     is_member: false,
+     permissions: ["*"],
+     except_permissions: ["#{Backoffice::AdminsController.controller_path}:write",
+                          "#{Backoffice::MembersController.controller_path}:write",
+                          "#{Backoffice::RolesController.controller_path}:write",
+                          "#{Backoffice::PermissionsController.controller_path}:write",
+                          "#{Backoffice::PermissionsController.controller_path}:write"] },
+    { name: "Member",
+      is_admin: false,
+      is_opt: false,
+      is_member: true,
+      permissions: ["#{Site::HomeController.controller_path}:read"],
+      except_permissions: ["*"] },
   ]
 
   # Print deprecation notices to the Rails logger.
