4a3408dcfc4066e6255116cd9ad6524749d15e0a
Guilherme Lúcio <guilhermecaixeta@users.noreply.github.com>
2024-03-29 20:13:57 +0000

    # Features: (#27)
    - Adding support to spin during the loads in site
    - adjust categories url to use description instead of the id
    - fix js methods to load changes after update the ajax events handlers

--- app/controllers/site/categories_controller.rb
+++ app/controllers/site/categories_controller.rb
@@ -1,7 +1,7 @@
 class Site::CategoriesController < SiteController
   def advertisements
-    @categories = Category.all.order(:id)
+    @categories = Category.all.order(:description)
     @category_description = params[:category_description]
-    @ads = Advertisement.by_category_description(params[:category_description])
+    @ads = Advertisement.by_category_description(@category_description)
   end
 end
--- app/webpacker/src/customs.js
+++ app/webpacker/src/customs.js
@@ -2,31 +2,38 @@ import { DataTable } from "simple-datatables.min.js";
 import { read } from "@popperjs/core";
 import Chart from "chart.js/auto";
 
+// based on this code https://stackoverflow.com/questions/799981/document-ready-equivalent-without-jquery
+var on_load = fn => document.readyState !== 'loading' ?
+    fn() :
+    document.addEventListener('turbolinks:load', fn);
+
+var on_request_complete = fn => on_load(() => {
+    document.addEventListener('ajax:complete', function (event) {
+        fn(event);
+    }, false)
+});
+
+window.DataTable = DataTable;
+window.Chart = Chart;
+window.read = read;
+window.on_load = on_load;
+window.on_request_complete = on_request_complete;
+
+on_request_complete(() => {
+    if (document.getElementById("datatablesSimple")) {
+        new DataTable("#datatablesSimple");
+    }
+});
+
 // Display errors in form when remote: 'true'.
 // To see more: https://github.com/turbolinks/turbolinks/issues/85#issuecomment-338784510
 // https://stackoverflow.com/questions/57935148/rails-6-form-with-remote-true-and-turbolinks-not-showing-flash-after-redirect
-document.addEventListener('ajax:complete', function (event) {
+on_request_complete((event) => {
     var xhr = event.detail[0]
     if ((xhr.getResponseHeader('Content-Type') || '').substring(0, 9) === 'text/html') {
         var referrer = window.location.href
         var snapshot = Turbolinks.Snapshot.wrap(xhr.response)
         Turbolinks.controller.cache.put(referrer, snapshot)
         Turbolinks.visit(referrer, { action: 'restore' })
-    }
-}, false)
-
-// based on this code https://stackoverflow.com/questions/799981/document-ready-equivalent-without-jquery
-var on_load = fn => document.readyState !== 'loading' ?
-    fn() :
-    document.addEventListener('turbolinks:load', fn);
-
-window.DataTable = DataTable;
-window.Chart = Chart;
-window.read = read;
-window.on_load = on_load;
-
-on_load(() => {
-    if (document.getElementById("datatablesSimple")) {
-        new DataTable("#datatablesSimple");
     }
 });
